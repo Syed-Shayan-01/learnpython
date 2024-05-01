a088e8b5621753be65b28f156345ef9a1036034f
Syed Shayan <shayansyed712@gmail.com>
2024-05-01 22:56:31 +0500

    Added and printed substrings of string variable 'a' using slicing
    * Defined a string variable 'a' with value 'syed shayan'
    * Printed substrings of 'a' using slicing:

--- firstcode.py
+++ firstcode.py
@@ -8,3 +8,12 @@
 # b = 2
 # print(a-b)
 
+
+
+a = 'syed shayan'
+
+print(a[0:1]) # length of the string
+print(a[0:4]) # 0 to 4 print value
+print(a[4:])  # 4 to end of the string print
+print(a[: 7]) # from start to 7th index
+print(a[0:len(a)])
