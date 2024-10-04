7b76d4512bf8f4f112b06e9128907333accbf0b4
Syed-Shayan-01 <shayansyed712@gmail.com>
2024-10-04 11:52:52 -0700

    "Deleted file array.py"

--- array.py
+++
@@ -1,7 +0,0 @@
-fruits = ["apple", "banana", "cherry"]
-name = input("Enter fruit name: ")
-
-if name in fruits:
-    print(name)
-else:
-    print("not in list")
---
+++ array_1.py
@@ -0,0 +1,33 @@
+# fruits = ["apple", "banana", "cherry"]
+# name = input("Enter fruit name: ")
+
+# if name in fruits:
+#     print(name)
+# else:
+#     print("not in list")
+
+
+
+fruits = ["apple", "banana", "cherry"]
+
+# fruits.append("mango")
+# print(fruits)
+
+# fruits.pop(2)
+# print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits)
+
+fruits.extend(["mango", "grapes", "ball"])
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+
+
+
